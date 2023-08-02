4a4f07ff9f54907844b4ee6987fde862a67db86c
yoonseokgyu <dbstjrrb1@gmail.com>
2023-08-02 17:13:52 +0900

    [feat] : Button 컴포넌트 생성

---
+++ src/components/buttons/button/index.tsx
@@ -0,0 +1,32 @@
+import { PropsWithChildren, type HTMLProps } from 'react';
+import style from './button.module.css';
+import cn from 'classnames';
+
+function Button({
+  type = 'button',
+  shape = 'flat',
+  className,
+  children,
+  ...restProps
+}: ButtonProps) {
+  const buttonStyle = cn(
+    style.button,
+    { [style.round]: shape === 'round' },
+    className
+  );
+
+  return (
+    <button type={type} className={buttonStyle} {...restProps}>
+      {children}
+    </button>
+  );
+}
+
+type ButtonProps = PropsWithChildren<{
+  type?: 'button' | 'submit' | 'reset';
+  shape?: 'flat' | 'round';
+  className?: string;
+}> &
+  HTMLProps<HTMLButtonElement>;
+
+export default Button;
