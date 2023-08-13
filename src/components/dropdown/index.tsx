331680e5af9d45eb24be305e4c1d0505df7b423f
yoonseokgyu <dbstjrrb1@gmail.com>
2023-08-13 16:52:53 +0900

    [feat] : Dropdown 컴포넌트 생성

---
+++ src/components/dropdown/index.tsx
@@ -0,0 +1,74 @@
+import { type HTMLProps } from 'react';
+import style from './dropdown.module.css';
+
+import cn from 'classnames';
+
+import ArrowDown from 'public/icons/arrow_down.svg';
+
+function DropDown({
+  id = 'text_input',
+  label,
+  error,
+  options,
+  disabled,
+  required,
+  className,
+  placeholder,
+  ...restProps
+}: DropDownProps) {
+  return (
+    <div>
+      <label
+        htmlFor={id}
+        className={cn(style.label, { [style.hidden]: !label })}>
+        {label}
+      </label>
+      <div
+        className={cn(
+          style.container,
+          {
+            [style.disabled]: disabled,
+            [style.errorBorder]: Boolean(error),
+          },
+          className
+        )}>
+        <select
+          id={id}
+          required
+          disabled={disabled}
+          className={style.select}
+          {...restProps}>
+          <option disabled selected value="" className={style.hidden}>
+            {placeholder}
+          </option>
+          {options.map(({ label, value }) => {
+            return (
+              <option key={`dropdown_item_${value}`} value={value}>
+                {label}
+              </option>
+            );
+          })}
+        </select>
+        <button tabIndex={-1} disabled={disabled} className={style.button}>
+          <ArrowDown />
+        </button>
+      </div>
+      {error && <span className={style.errorText}>{error}</span>}
+    </div>
+  );
+}
+
+type Option = {
+  label: string;
+  value: string;
+};
+
+type DropDownProps = {
+  id?: string;
+  label?: string;
+  error?: string;
+  options: Option[];
+  placeholder: string;
+} & HTMLProps<HTMLSelectElement>;
+
+export default DropDown;
