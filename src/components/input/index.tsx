f5a6bf79105748025809be3562d49647999070af
yoonseokgyu <dbstjrrb1@gmail.com>
2023-08-13 17:41:23 +0900

    [feat] : 스타일 수정
    - label, cancel 버튼 스타일 수정
    - 제어 컴포넌트 형태로 cancel 버튼 구현

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,30 +1,59 @@
-import { type HTMLProps } from 'react';
+import { useState, type HTMLProps, forwardRef } from 'react';
 import style from './input.module.css';
 
 import cn from 'classnames';
 
-function Input({
-  id = 'text_input',
-  type = 'search',
-  label,
-  error,
-  ...restProps
-}: InputProps) {
-  return (
-    <div>
-      <label htmlFor={id} className={cn({ [style.hidden]: Boolean(label) })}>
-        {label}
-      </label>
-      <div
-        className={cn(style.container, {
-          [style.errorBorder]: Boolean(error),
-        })}>
-        <input id={id} {...restProps} />
+import Cancel from 'public/icons/cancel.svg';
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    { id = 'text_input', label, error, className, ...restProps }: InputProps,
+    ref
+  ) => {
+    const [value, setValue] = useState('');
+    const [isFocus, setFocus] = useState(false);
+
+    return (
+      <div>
+        <label
+          htmlFor={id}
+          className={cn(style.label, { [style.hidden]: !label })}>
+          {label}
+        </label>
+        <div
+          className={cn(
+            style.container,
+            {
+              [style.errorBorder]: Boolean(error),
+            },
+            className
+          )}>
+          <input
+            id={id}
+            ref={ref}
+            value={value}
+            autoComplete="off"
+            className={style.input}
+            onBlur={() => setFocus(false)}
+            onFocus={() => setFocus(true)}
+            onChange={(e) => setValue(e.target.value)}
+            {...restProps}
+          />
+          <button
+            onMouseDown={() => setValue('')}
+            className={cn(style.button, {
+              [style.hidden]: !value || !isFocus,
+            })}>
+            <Cancel />
+          </button>
+        </div>
+        {error && <span className={style.errorText}>{error}</span>}
       </div>
-      {error && <span className={style.errorText}>{error}</span>}
-    </div>
-  );
-}
+    );
+  }
+);
+
+Input.displayName = 'Input';
 
 type InputProps = {
   id?: string;
