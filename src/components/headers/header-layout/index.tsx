b7e4c281bcd9a6a6fc529164231a10917df6ad66
yoonseokgyu <dbstjrrb1@gmail.com>
2023-08-27 00:27:12 +0900

    [fix] : Header Layout classNames Props 추가

--- src/components/headers/header-layout/index.tsx
+++ src/components/headers/header-layout/index.tsx
@@ -2,10 +2,12 @@ import { PropsWithChildren } from 'react';
 
 import style from './header.module.css';
 
-const HeaderLayout = ({ children }: HeaderProps) => {
-  return <header className={style.header}>{children}</header>;
+import cn from 'classnames';
+
+const HeaderLayout = ({ children, className }: HeaderProps) => {
+  return <header className={cn(style.header, className)}>{children}</header>;
 };
 
-type HeaderProps = PropsWithChildren<{}>;
+type HeaderProps = PropsWithChildren<{ className?: string }>;
 
 export default HeaderLayout;
