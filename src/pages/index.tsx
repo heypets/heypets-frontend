d76ec09d90367a3e500aabd75d58447ecd032f2b
yoonseokgyu <dbstjrrb1@gmail.com>
2023-09-16 14:50:16 +0900

    [fix] : 캘린더 컴포넌트 위치 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import PetList from '@/components/page/home/petlist';
 import HomeHeader from '@/components/headers/home';
 
 import style from '@styles/home.module.css';
-import Calendar from '@/components/calendar';
+import Calendar from '@/components/page/home/calendar';
 
 const Home = () => {
   return (
