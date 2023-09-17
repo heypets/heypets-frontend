1674bb73dbf77a94231831853797af9a2a43f6e1
yoonseokgyu <dbstjrrb1@gmail.com>
2023-09-17 19:16:49 +0900

    [fix] : Tabs 컴포넌트
    - Tabs 컴포넌트 className Props 추가
    - 스타일 관련 수정

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,33 +1,36 @@
-import cn from "classnames";
-import Link from "next/link";
-import style from "./Tabs.module.css";
-import { usePathname, useSearchParams } from "next/navigation";
+import cn from 'classnames';
+import Link from 'next/link';
+import style from './Tabs.module.css';
+import { usePathname, useSearchParams } from 'next/navigation';
 import {
   Children,
   type PropsWithChildren,
   ReactElement,
   cloneElement,
   isValidElement,
-} from "react";
+} from 'react';
 
 export type StrictPropsWithChildren<P = unknown> = P & {
   children: React.ReactNode;
 };
 
-export default function Tabs({ children }: StrictPropsWithChildren) {
-  return <div>{children}</div>;
+export default function Tabs({
+  className,
+  children,
+}: StrictPropsWithChildren<{ className?: string }>) {
+  return <div className={className}>{children}</div>;
 }
 
 const renderTabElement = (
   elements: ReactElement[],
-  props: Array<React.ComponentProps<typeof Tab>>,
+  props: Array<React.ComponentProps<typeof Tab>>
 ) => {
   if (elements.length === 1) {
     return elements[0];
   }
 
   return (
-    <div className={style["tabs"]}>
+    <div className={style['tabs']}>
       {elements.map((element, index) => {
         return cloneElement(element);
       })}
@@ -35,15 +38,13 @@ const renderTabElement = (
   );
 };
 
-function List({
-  children,
-}: StrictPropsWithChildren) {
+function List({ children }: StrictPropsWithChildren) {
   const validChildren = Children.toArray(children).filter((child) =>
     isValidElement(child)
   ) as ReactElement[];
 
   if (validChildren.length === 0) {
-    throw new Error("List 컴포넌트는 Tab 컴포넌트를 포함해야 합니다.");
+    throw new Error('List 컴포넌트는 Tab 컴포넌트를 포함해야 합니다.');
   }
 
   const props = validChildren.map(
@@ -59,23 +60,22 @@ interface TabProps {
   queryString?: string;
 }
 
-function Tab({ value, text, queryString}: TabProps) {
+function Tab({ value, text, queryString }: TabProps) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
-  const isActive = searchParams.get("tab") === value;
+  const isActive = searchParams.get('tab') === value;
 
   return (
     <Link
-      className={cn("sub_head_3", style["tab"], {
-        [style["tab-active"]]: isActive,
+      className={cn('sub_head_3', style['tab'], {
+        [style['tab-active']]: isActive,
       })}
       href={{
         pathname,
         query: { tab: queryString ?? value },
       }}
-      replace
-    >
+      replace>
       {text}
     </Link>
   );
@@ -84,9 +84,9 @@ function Tab({ value, text, queryString}: TabProps) {
 function Panel({
   value,
   children,
-}: PropsWithChildren<Pick<TabProps, "value">>) {
+}: PropsWithChildren<Pick<TabProps, 'value'>>) {
   const searchParams = useSearchParams();
-  const isActive = searchParams.get("tab") === value;
+  const isActive = searchParams.get('tab') === value;
 
   return isActive && children;
 }
