1b28f17eb2e2b383b5478b4c4ca90f56386843ba
yoonseokgyu <dbstjrrb1@gmail.com>
2023-09-23 16:09:01 +0900

    [feat]: home 페이지 컴포넌트 업데이트

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,12 +3,19 @@ import HomeHeader from '@/components/headers/home';
 
 import style from '@styles/home.module.css';
 import Calendar from '@/components/page/home/calendar';
+import Today from '@/components/page/home/today';
+import Terms from '@/components/page/home/terms';
 
 const Home = () => {
   return (
     <div className={style.wrapper}>
       <PetList className={style.petlist} />
+
       <Calendar className={style.calendar} />
+
+      <Today className={style.today} />
+
+      <Terms />
     </div>
   );
 };
